4a2955d5008d9c06d10d2a9af299ee4839a01f5b
xdan <chupurnov@gmail.com>
2023-08-25 13:50:04 +0300

    Initial commit

---
+++ example/common.js
@@ -0,0 +1,23 @@
+mappable.import.loaders.unshift(async (pkg) => {
+    if (!pkg.includes('@mappable-world/mappable-entity-tile-loader')) {
+        return;
+    }
+
+    if (location.href.includes('localhost')) {
+        await mappable.import.script(`/dist/index.js`);
+    } else {
+        await mappable.import.script(`https://unpkg.com/${pkg}/dist/index.js`);
+    }
+
+    Object.assign(mappable, window[`${pkg}`]);
+    return window[`${pkg}`];
+})
+
+
+const BOUNDS = [
+    [54.58311, 25.99850],
+    [56.30248, 24.47889]
+];
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const LOCATION = {bounds: BOUNDS};
